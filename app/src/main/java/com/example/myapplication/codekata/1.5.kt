b87b22d291653ac143bc7eb82485ab16b4f89014
distecter <152145394+distecter@users.noreply.github.com>
2023-12-14 09:52:01 +0900

    1.5

---
+++ app/src/main/java/com/example/myapplication/codekata/1.5.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.codekata
+
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        var answer: Int = 0;
+        if(num1 == num2) answer=1;
+        else answer=-1;
+        return answer
+    }
+}
